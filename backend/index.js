92eebb94aa5596e0a7f0b2aef78cc96d6fc3c8b1
monsoonssn <22012050+monsoonssn@users.noreply.github.com>
2022-04-12 18:05:19 -0600

    added some routes for api

--- backend/index.js
+++ backend/index.js
@@ -7,44 +7,251 @@ app.use(express.json());
 
 const prisma = new PrismaClient();
 
-app.get("/api/client/", async (_, res) => {
-  const clients = await prisma.client.findMany();
+app.get("/api/client", async (req, res) => {
+  const agentIdQuery = req.query.agent_id;
+  let clients;
+
+  if (agentIdQuery) {
+    clients = await prisma.client.findMany({
+      where: {
+        agent_id: Number(agentIdQuery),
+      },
+    });
+  } else {
+    clients = await prisma.client.findMany();
+  }
+
   res.json(clients);
 });
 
-app.post("/api/client/", async (req, res) => {
+app.post("/api/client", async (req, res) => {
   const { email, fname, lname, phone_num, agent_email } = req.body;
-  const client = await prisma.client.create({
-    data: {
-      email,
-      fname,
-      lname,
-      phone_num,
-      agent_email,
-    },
-  }).catch((err) => {
-    res.status(400).send({ error: "bad request" });
-  });
+  const client = await prisma.client
+    .create({
+      data: {
+        email,
+        fname,
+        lname,
+        phone_num,
+        agent_email,
+      },
+    })
+    .catch(_ => {
+      res.sendStatus(400);
+    });
 
   res.json(client);
 });
 
-app.put("/api/client/:email", async (req, res) => {
-  const { email } = req.params;
-  const { fname, lname, phone_num, agent_email } = req.body;
-  const client = await prisma.client.update({
-    where: {
-      email,
-    },
-    data: {
-      fname,
-      lname,
-      phone_num,
-      agent_email,
-    },
-  });
+app.put("/api/client/:id", async (req, res) => {
+  const { id } = req.params;
+  const { email, fname, lname, phone_num, agent_id } = req.body;
+  const client = await prisma.client
+    .update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        email,
+        fname,
+        lname,
+        phone_num,
+        agent_id,
+      },
+    })
+    .catch(_ => {
+      res.sendStatus(400);
+    });
 
   res.json(client);
+});
+
+app.get("api/client/:id", async (req, res) => {
+  const { id } = req.params;
+  const client = await prisma.client
+    .findOne({
+      where: {
+        id: Number(id),
+      },
+    })
+    .catch(_ => {
+      res.sendStatus(400);
+    });
+
+  res.json(client);
+});
+
+app.get("/api/agent", async (_, res) => {
+  const agents = await prisma.agent.findMany();
+  res.json(agents);
+});
+
+app.post("/api/agent", async (req, res) => {
+  const { email, fname, lname, phone_num, city } = req.body;
+  const agent = await prisma.agent
+    .create({
+      data: {
+        email,
+        fname,
+        lname,
+        phone_num,
+        city,
+      },
+    })
+    .catch(_ => {
+      res.sendStatus(400);
+    });
+
+  res.json(agent);
+});
+
+app.put("/api/agent/:id", async (req, res) => {
+  const { id } = req.params;
+  const { email, fname, lname, phone_num, city } = req.body;
+  const agent = await prisma.agent
+    .update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        email,
+        fname,
+        lname,
+        phone_num,
+        city,
+      },
+    })
+    .catch(_ => {
+      res.sendStatus(400);
+    });
+
+  res.json(agent);
+});
+
+app.get("/api/buyer", async (_, res) => {
+  const buyers = await prisma.buyer.findMany();
+  res.json(buyers);
+});
+
+app.post("/api/buyer", async (req, res) => {
+  const { id, budget, type, requirement_id } = req.body;
+  const buyer = await prisma.buyer
+    .create({
+      data: {
+        id,
+        budget,
+        type,
+        requirement_id,
+      },
+    })
+    .catch(_ => {
+      res.sendStatus(400);
+    });
+
+  res.json(buyer);
+});
+
+app.put("/api/buyer/:id", async (req, res) => {
+  const { id } = req.params;
+  const { budget, type, requirement_id } = req.body;
+  const buyer = await prisma.buyer
+    .update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        budget,
+        type,
+        requirement_id,
+      },
+    })
+    .catch(_ => {
+      res.sendStatus(400);
+    });
+
+  res.json(buyer);
+});
+
+app.get("/api/seller", async (_, res) => {
+  const sellers = await prisma.seller.findMany();
+  res.json(sellers);
+});
+
+app.post("/api/seller", async (req, res) => {
+  const { id, type } = req.body;
+  const seller = await prisma.seller
+    .create({
+      data: {
+        id,
+        type,
+      },
+    })
+    .catch(_ => {
+      res.sendStatus(400);
+    });
+
+  res.json(seller);
+});
+
+app.put("/api/seller/:id", async (req, res) => {
+  const { id } = req.params;
+  const { type } = req.body;
+  const seller = await prisma.seller
+    .update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        type,
+      },
+    })
+    .catch(_ => {
+      res.sendStatus(400);
+    });
+
+  res.json(seller);
+});
+
+app.get("/api/renter", async (_, res) => {
+  const renters = await prisma.renter.findMany();
+  res.json(renters);
+});
+
+app.post("/api/renter", async (req, res) => {
+  const { id, type, requirement_id } = req.body;
+  const renter = await prisma.renter
+    .create({
+      data: {
+        id,
+        type,
+        requirement_id,
+      },
+    })
+    .catch(_ => {
+      res.sendStatus(400);
+    });
+
+  res.json(renter);
+});
+
+app.put("/api/renter/:id", async (req, res) => {
+  const { id } = req.params;
+  const { type, requirement_id } = req.body;
+  const renter = await prisma.renter
+    .update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        type,
+        requirement_id,
+      },
+    })
+    .catch(_ => {
+      res.sendStatus(400);
+    });
+
+  res.json(renter);
 });
 
 const unknownEndpoint = (_, res) => {
