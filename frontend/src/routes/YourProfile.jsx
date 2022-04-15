8bfcd2c57f12efcb51d47f9c58324ee5d4ffe53f
monsoonssn <22012050+monsoonssn@users.noreply.github.com>
2022-04-14 18:36:43 -0600

    stuff

--- frontend/src/routes/YourProfile.jsx
+++ frontend/src/routes/YourProfile.jsx
@@ -1,114 +1,90 @@
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
 
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import HomeIcon from '@mui/icons-material/Home';
-import useStyles from '../jstyles.jsx';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import CardActions from '@mui/material/CardActions';
+const BASE_URL = "localhost:3001";
 
-// const AgentProfile = () => (
-//   <div className="agentProfile">
-//     <div className="agentProfile_content">
-//       <h1>Your Profile</h1>
-//       <Typography variant = "h2"> Name: AJohn Smith</Typography>
-//       <p>Name: AJohn Smith</p>
-//       <p>Phone Number: (555) 555 - 5555</p>
-//       <p>Email: john.smith@ucalgary.ca</p>
-//       <p>City: Calgary</p>
-//       <div className="agentProfile_btns">
-//         <Link to="/editProfile">
-//           <Button label="Edit" />
-//         </Link>
-
-//       </div>
-//     </div>
-//   </div>
-// );
-
-const cards = [1]
+const cards = [1];
 
 const YourProfile = () => {
-  //const classes = useStyles();
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [userData, setUserData] = React.useState({});
+
+  const getProfileInformation = () => {
+    axios.get(`http://${BASE_URL}/api/agent/${user.id}`).then(res => {
+      console.log(res.data);
+      setUserData(res.data);
+    });
+  };
+
   return (
     <>
       <CssBaseline />
       <main>
-        <div > 
-          {/* className={classes.container} */}
-          <Container maxWidth = "sm">
-            <Typography variant="h2" align= "center" color ="textPrimary" gutterBottom>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
               Your Profile
             </Typography>
-            <Typography variant="h5" align= "center" color ="textSecondary" paragraph>
-              Hello! Welcome to your profile. Here you can view and edit your information.
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello! Welcome to your profile. Here you can view and edit your
+              information.
             </Typography>
           </Container>
         </div>
 
-        <Container maxWidth="xl" style={{
-          padding: '20pxx 0'}}> 
+        <Container
+          maxWidth="xl"
+          style={{
+            padding: "20pxx 0",
+          }}
+        >
           <Grid container spacing={4}>
             {cards.map(() => (
               <Grid item key={Card} xs={12} sm={6} md={4}>
-              <Card style={{
-                height: '100%',
-                display: 'flex',
-                flexDirection: 'column'
-              }}> 
-                  <CardContent style={{
-                    flexGrow: 1
-                  }} > 
+                <Card
+                  style={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardContent
+                    style={{
+                      flexGrow: 1,
+                    }}
+                  >
                     <Typography gutterBottom variant="h5">
-                      Name: John Cena
+                      Name: {userData.fname} {userData.lname}
                     </Typography>
-                    <Typography>
-                      Phone: 555 555 5555
-                    </Typography>
-                    <Typography>                      
-                      Email: john.cena@live.com
-                    </Typography>
-                    <Typography>
-                      City: Calgary
-                    </Typography>
+                    <Typography>Phone: {userData.phone_num}</Typography>
+                    <Typography>Email: {userData.email}</Typography>
                   </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary"> Edit</Button>
-                  </CardActions>
-              </Card>
-            </Grid>
+                  {/* <CardActions></CardActions> */}
+                </Card>
+              </Grid>
             ))}
           </Grid>
-
-        </Container> 
+        </Container>
       </main>
-      {/* <footer style ={{padding: '50px 0'}} > 
-        <Typography variant="h6" align= "center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" color="textSeconday">
-          Something here to give the footer a purpose!
-        </Typography>
-      </footer> */}
-
     </>
   );
-}
+};
 
 export default YourProfile;
